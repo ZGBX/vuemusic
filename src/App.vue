d6e7d2f7d6dfaa07259fac7cf1c94f1287a74033
zgbx <2399535375@qq.com>
2020-05-28 15:31:49 +0800

    修改

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
